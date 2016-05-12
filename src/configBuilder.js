95b499f781b42af35af6f92459689114d4e17395
Nicolasribot <nicolas.ribot@gmail.com>
2016-05-12 14:04:56 +0200

    first method to get options from leaflet-wms source

--- src/configBuilder.js
+++ src/configBuilder.js
@@ -3,6 +3,12 @@ var iconLayersConfigBuilder = {
     // uses a leaflet-wms objct (see @https://github.com/Nicolasribot/leaflet.wms)
     'buildFromWMSSource': function (source) {
         console.log(source);
+        var ret = [{}];
+        if (source) {
+            return source.getLayersForControl();
+        }
+        
+        return ret;
     },
     
     // uses a classic WMS leaflet layer
